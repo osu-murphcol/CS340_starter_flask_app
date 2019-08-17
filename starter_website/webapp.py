ae1f094d0d56f33221438240e1f06d44c379bf63
Colin Murphy <murphcol@oregonstate.edu>
2019-08-16 20:10:20 -0700

    fix regex

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -268,9 +268,9 @@ def change_address():
         query = 'SELECT * FROM Final_Users WHERE email = \'%s\'' % (email)
         user = execute_query(db_connection, query).fetchone()
         if request.method=='POST' and form.validate():
+            flash('Address updated!')
             query = 'UPDATE Final_Addresses SET street = \'%s\', zip = \'%s\', city = \'%s\', state = \'%s\' WHERE email = \'%s\'' % (form.street.data, form.zip_code.data, form.city.data, form.state.data, email)
             execute_query(db_connection, query) 
-            flash('Address updated!')
         elif request.method=='GET':
             query = 'SELECT street, zip, city, state FROM Final_Addresses WHERE email = \'%s\'' % (email)
             address = execute_query(db_connection, query).fetchone()
