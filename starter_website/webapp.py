6acfe91cf4586a925f1a01f4d2c252ebe5731b22
Colin Murphy <murphcol@oregonstate.edu>
2019-08-16 19:49:42 -0700

    fix regex

--- starter_website/webapp.py
+++ starter_website/webapp.py
@@ -271,19 +271,18 @@ def change_address():
         email = session['email']
         query = 'SELECT * FROM Final_Users WHERE email = \'%s\'' % (email)
         user = execute_query(db_connection, query).fetchone()
-        if request.method=='GET':
-            query = 'SELECT street, zip, city, state FROM Final_Addresses WHERE email = \'%s\'' % (email)
-            address = execute_query(db_connection, query).fetchone()
-            form.street.data = address[0]
-            form.zip_code.data = address[1]
-            form.city.data = address[2]
-            form.state.data = address[3]
-            return render_template('change_address.html', form=form, user=user)
         if request.method=='POST':
             if form.validate():
                 query = 'UPDATE Final_Addresses SET street = \'%s\', zip = \'%s\', city = \'%s\', state = \'%s\' WHERE email = \'%s\'' % (form.street.data, form.zip_code.data, form.city.data, form.state.data, email)
                 execute_query(db_connection, query) 
                 flash('Address updated!')
+        elif request.method=='GET':
+            query = 'SELECT street, zip, city, state FROM Final_Addresses WHERE email = \'%s\'' % (email)
+            address = execute_query(db_connection, query).fetchone()
+            form.street.data = address[0]
+            form.zip_code.data = address[1]
+            form.city.data = address[2]
+            form.state.data = address[3]=
         return render_template('change_address.html', form=form, user=user)
     return redirect(url_for('login'))   
 
